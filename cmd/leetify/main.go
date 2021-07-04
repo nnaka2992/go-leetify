5b575ac43018485b59e9625cf9b57e7c111bab52
nnaka2992 <nnaka2992@gmail.com>
2021-07-04 19:06:08 +0900

    add functionality to show conversion rule\nadd functionality to only apply char or word

--- cmd/leetify/main.go
+++ cmd/leetify/main.go
@@ -12,20 +12,47 @@ import (
 )
 
 func main() {
-	//random := flag.Bool("r", false, "Randomly apply leetify")
-	show_input := flag.Bool("s", false, "Show input")
+	showInput := flag.Bool("s", false, "Show input")
+	showCharList := flag.Bool("list-characters", false, "List characters' conversion table")
+	showWordList := flag.Bool("list-words", false, "List words' conversion table")
+	applyChar := flag.Bool("c", true, "Change characters")
+	applyWord := flag.Bool("w", true, "Change words")
 	flag.Parse()
 
 	rand.Seed(time.Now().UTC().UnixNano())
+	if *showCharList || *showWordList {
+		if *showCharList {
+			leet.PrintChatTable()
+		}
+		if *showWordList {
+			leet.PrintWordTable()
+		}
+		os.Exit(0)
+	}
+
+	if !(*applyChar || *applyWord) {
+		fmt.Fprintf(os.Stderr, "One of c or w must be true")
+		os.Exit(1)
+	}
 
     s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
 		word := string(s.Text())
-		new_word := leet.ApplyLeet(word)
-		if *show_input {
-			fmt.Printf("%v: %v\n", word, new_word)
+		newWord := ""
+		if *applyChar != *applyWord {
+			if *applyChar {
+				newWord = leet.ApplyChar(word)
+			} else if *applyWord {
+				newWord = leet.ApplyWord(word)
+			}
 		} else {
-			fmt.Println(new_word)
+			newWord = leet.ApplyLeet(word)
+		}
+
+		if *showInput {
+			fmt.Printf("%v: %v\n", word, newWord)
+		} else {
+			fmt.Println(newWord)
 		}
 	}
 
